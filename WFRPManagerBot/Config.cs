a29d18dc7d9fe7a96cfd6f1750a558d6f7da075e
SentiWW <sentiww@gmail.com>
2020-10-13 16:35:21 +0200

    Add project files.

---
+++ WFRPManagerBot/Config.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WFRPManagerBot
+{
+    class Config
+    {
+        private const string configFolder = "Resources";
+        private const string configFile = "/config.xml";
+        private const string path = configFolder + configFile;
+
+        public static BotConfig bot;
+
+        static Config()
+        {
+            if (!Directory.Exists(configFolder)) Directory.CreateDirectory(configFolder);
+
+            if (!File.Exists(path))
+            {
+                bot = new BotConfig();
+
+                var xmlSerializer = new XmlSerializer(typeof(BotConfig));
+                var file = File.Create(path);
+                xmlSerializer.Serialize(file, bot);
+                file.Close();
+            }
+            else
+            {
+                var xmlSerializer = new XmlSerializer(typeof(BotConfig));
+                using(Stream reader = new FileStream(path, FileMode.Open))
+                {
+                    bot = (BotConfig)xmlSerializer.Deserialize(reader);
+                }
+            }
+        }
+    }
+
+    public class BotConfig
+    {
+        public string token = "YourToken";
+        public string cmdPrefix = "YourCommandPrefix";
+    }
+}
---
+++ WFRPManagerBot/Core/Commands.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Resources;
+using System.Reflection;
+
+namespace WFRPManagerBot.Core
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        public async Task Help()
+        {
+            await Context.Channel.SendMessageAsync(commands.Help);
+        }
+    }
+}
